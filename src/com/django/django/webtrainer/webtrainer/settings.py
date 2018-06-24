07ff2b3125b2ce71833e1cfe08e05721b9c38f50
whliu <whliu91@gmail.com>
2018-06-24 12:44:30 +1000

    add pyc to ignore

--- src/com/django/django/webtrainer/webtrainer/settings.py
+++ src/com/django/django/webtrainer/webtrainer/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'webtrainer.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'invincible',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
